0b465b7c302fa39ea6c2c31891a2c2fdf8281892
Noman-Nom <noman5456318@gmail.com>
2023-11-24 23:45:29 +0500

    question five completed

---
+++ Easy(1-30)/Five.js
@@ -0,0 +1,17 @@
+// 5. What is the purpose of the `this` keyword?
+
+// The this keyword in JavaScript is a reference to the object on which the current code is being executed. Its value is determined by how a function is called, and it provides a way to access the properties and methods of the object to which it belongs. The behavior of this can vary depending on the context in which it is used.
+
+
+const Object = {
+    name: "Muhammad Noman",
+    message: function(){
+        // console.log(this.name + ` Hello`)
+        // console.log(this.name, ` Hello`)
+        console.log(`${this.name} Hello`)
+    }
+
+
+
+}
+Object.message()
